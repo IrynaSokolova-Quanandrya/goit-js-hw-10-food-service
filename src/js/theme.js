c4191c70b464c410457c280881788feb359b7e8a
Iryna <irasokolova25@ukr.net>
2021-08-21 00:25:50 +0300

    Update Theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,29 +1,22 @@
-
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 const body = document.querySelector('body');
-console.log(body);
-
 const input = document.getElementById('theme-switch-toggle');
-input.addEventListener('change', onDarckTheme);
 
-input.getAttribute("checked");
-
-function onDarckTheme() {
-    const darckTheme = {
-        theme: 'darck',
-        checked: 'true',
-        change: 'on'
+input.addEventListener('change', () => {
+    const theme = body.classList.toggle('dark-theme');
+    theme?input.setAttribute('checked', ''):input.removeAttribute('checked');
+    localStorage.setItem('theme', 'dark-theme');
+} )
+ 
+function savedTheme() {
+    if (localStorage.getItem('theme', 'dark-theme')) {
+        input.setAttribute('checked', '');
+    }
+    
 }
 
-    body.classList.toggle('dark-theme');
-    input.getAttribute("checked", "true");
-    localStorage.setItem('theme', 'DARK');
-}
-
-
-
-
-localStorage.getItem('theme');
-
-// console.log(localStorage);
-
+  
