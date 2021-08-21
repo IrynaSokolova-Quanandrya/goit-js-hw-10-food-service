35037ef0cb33c92f4b34b80706263e7f81e79f52
IrynaSokolova-Quanandrya <80633345+IrynaSokolova-Quanandrya@users.noreply.github.com>
2021-08-21 05:47:27 +0000

    Deploying to gh-pages from @ IrynaSokolova-Quanandrya/goit-js-hw-10-food-service@e95a405f2dd76b09053ee44980083dbe431fe84e 🚀

--- src.4992b778.js
+++ src.4992b778.js
@@ -11,8 +11,8 @@ var e,t=arguments[3];!function(t,r){"object"==typeof exports&&"object"==typeof m
 },{"handlebars/dist/handlebars.runtime":"pAws"}],"i0CD":[function(require,module,exports) {
 "use strict";var e=u(require("./menu.json")),t=u(require("../templates/menu.hbs"));function u(e){return e&&e.__esModule?e:{default:e}}const r=document.querySelector(".js-menu"),n=(0,t.default)(e.default),o=document.querySelector(".tag-list"),s=document.querySelector(".tag-list__item");r.insertAdjacentHTML("beforeend",n);
 },{"./menu.json":"NkM5","../templates/menu.hbs":"dgbP"}],"qwQt":[function(require,module,exports) {
-const e={LIGHT:"light-theme",DARK:"dark-theme"},t=document.querySelector("body"),c=document.getElementById("theme-switch-toggle");function h(){localStorage.getItem("theme","dark-theme")&&c.setAttribute("checked","")}c.addEventListener("change",()=>{t.classList.toggle("dark-theme")?c.setAttribute("checked",""):c.removeAttribute("checked"),localStorage.setItem("theme","dark-theme")});
+const e={LIGHT:"light-theme",DARK:"dark-theme"},t=document.querySelector("body"),c=document.getElementById("theme-switch-toggle");function m(){t.classList.toggle("dark-theme")?c.setAttribute("checked",""):c.removeAttribute("checked"),localStorage.setItem("theme","dark-theme")}function o(){localStorage.getItem("theme","dark-theme")&&m()}o(),c.addEventListener("change",m);
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/menu"),require("./js/theme");
 },{"./sass/main.scss":"clu1","./js/menu":"i0CD","./js/theme":"qwQt"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-10-food-service/src.83b29152.js.map
+//# sourceMappingURL=/goit-js-hw-10-food-service/src.4992b778.js.map
