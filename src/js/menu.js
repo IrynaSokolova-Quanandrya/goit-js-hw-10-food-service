1b18ce8fd08d3052f23d0f2eb02bc75132b6846b
Iryna <irasokolova25@ukr.net>
2021-08-21 21:17:19 +0300

    Update marckup

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,16 +2,9 @@ import menuData from './menu.json';
 import menuCardTemplate from '../templates/menu.hbs';
 
 const menu = document.querySelector('.js-menu');
+
 const menuCardMarckup = menuCardTemplate(menuData);
-const tagList = document.querySelector('.tag-list');
-const tagItem = document.querySelector('.tag-list__item');
 menu.insertAdjacentHTML('beforeend', menuCardMarckup);
 
 
 
-// const ingredient =  menuData.ingredients.map(elem => {
-//   tagItem.textContent = elem;
-//   return;
- 
-// })
-//  tagList.append(...ingredient);
