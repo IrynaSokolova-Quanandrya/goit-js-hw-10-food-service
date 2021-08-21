e95a405f2dd76b09053ee44980083dbe431fe84e
Iryna <irasokolova25@ukr.net>
2021-08-21 08:46:44 +0300

    Added saved theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -5,16 +5,18 @@ const Theme = {
 
 const body = document.querySelector('body');
 const input = document.getElementById('theme-switch-toggle');
+savedTheme();
 
-input.addEventListener('change', () => {
-    const theme = body.classList.toggle('dark-theme');
-    theme?input.setAttribute('checked', ''):input.removeAttribute('checked');
-    localStorage.setItem('theme', 'dark-theme');
-} )
+input.addEventListener('change', onInputChange);
+    function onInputChange() {
+        const theme = body.classList.toggle('dark-theme');
+        theme ? input.setAttribute('checked', '') : input.removeAttribute('checked');
+        localStorage.setItem('theme', 'dark-theme');
+    };
  
 function savedTheme() {
     if (localStorage.getItem('theme', 'dark-theme')) {
-        input.setAttribute('checked', '');
+        onInputChange();
     }
     
 }
