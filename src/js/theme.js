75824833641ea81f444bdd2603b183f32b46509a
Iryna <irasokolova25@ukr.net>
2021-08-25 11:46:06 +0300

    Update theme.js

--- src/js/theme.js
+++ src/js/theme.js
@@ -7,21 +7,22 @@ const body = document.querySelector('body');
 body.classList.toggle(Theme.LIGHT);
 
 const input = document.getElementById('theme-switch-toggle');
-savedTheme();
+
 
 input.addEventListener('change', onInputChange);
 function onInputChange() {
         
         const theme = body.classList.toggle(Theme.DARK);
         theme ? input.setAttribute('checked', '') : input.removeAttribute('checked');
-        localStorage.setItem('theme', 'Theme.DARK');
-    };
+    localStorage.setItem('theme', 'Theme.DARK');
+    
+};
  
 function savedTheme() {
     if (localStorage.getItem('theme', 'Theme.DARK')) {
         onInputChange();
     }
-    
+    localStorage.removeItem('theme');
 }
-
+savedTheme();
   
